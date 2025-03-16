2c5deba9fade64a43b1867539b23dac1876856f8
bmalkes <bmalkes@gmail.com>
2025-03-16 13:40:35 +0200

    feat:add new url deploy

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //placing user order for frontend
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://tomato-ten-snowy.vercel.app/";
   try {
     // Verificar se todos os campos necessários existem
     if (
